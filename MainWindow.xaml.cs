cca10de0531eaa2c7edd9ca1b3d141df0f535505
maxxxel <m.gromball@gmx.de>
2022-03-27 12:27:41 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.IO;
+
+namespace SR_ImpEx
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportDRSFile(string fileName)
+        {
+
+        }
+
+        private void ImportGLTFFile(string fileName)
+        {
+
+        }
+
+        private void ImportFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog OpenFileDialog = new OpenFileDialog();
+            OpenFileDialog.Filter = "All files|*.glb;*.gltf;*.drs|gLTF|*.glb;*.gltf|Battleforge DRS|*.drs";
+
+            if (OpenFileDialog.ShowDialog() == true)
+            {
+                string Extension = Path.GetExtension(OpenFileDialog.FileName).ToUpperInvariant();
+
+                if (Extension == ".DRS")
+                {
+                    ImportDRSFile(OpenFileDialog.FileName);
+                }
+                else if (Extension == ".GLB" || Extension == ".GLTF")
+                {
+                    ImportGLTFFile(OpenFileDialog.FileName);
+                }
+            }
+        }
+    }
+}
